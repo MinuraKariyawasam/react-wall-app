4360ed12cc7bb57e7ff6689180c5b7e7c2e41591
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-02-01 21:33:17 +0530

    update course content part

--- components/CourseContent/index.js
+++ components/CourseContent/index.js
@@ -5,8 +5,18 @@ import {Text, View } from 'react-native';
 const CourseContent = (props) => {
     return (
 
-        <View>
-            Hello Course Content
+        <View style={course.container}>
+            
+            <ImageBackground source = {require('../../assets/images/main01.jpg')} style={styles.image}/>
+        
+        	<View style={styles.titles}>
+        
+            <Text style={styles.title}>Course Content</Text>
+        
+            <Text style={styles.subTitle}>Follow the correct order</Text>
+
+          </View>
+
         </View>
 
     )
